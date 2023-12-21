a81a479dabde39dca38512c635c8d0ef5aa2c2b6
yxxd <=131183692+free-zhang@user.noreply.gitee.com>
2023-12-21 11:06:15 +0800

    实体类

---
+++ src/main/java/com/score/bean/CanteenManager.java
@@ -0,0 +1,66 @@
+package com.score.bean;
+
+import java.io.Serializable;
+
+public class CanteenManager implements Serializable {
+    private int id;
+    private String name;
+    private String gender;
+    private String password;
+    private int canteenId;
+
+    public CanteenManager() {
+        // 默认构造函数
+    }
+
+    public CanteenManager(int id, String name, String gender, String password, int canteenId) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.password = password;
+        this.canteenId = canteenId;
+    }
+
+    // getter和setter方法
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCanteenId() {
+        return canteenId;
+    }
+
+    public void setCanteenId(int canteenId) {
+        this.canteenId = canteenId;
+    }
+}
+
---
+++ src/main/java/com/score/bean/Complaint.java
@@ -0,0 +1,108 @@
+package com.score.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Complaint implements Serializable {
+    private int id;
+    private int complainerId;
+    private int canteenId;
+    private String complaintInfo;
+    private Date complaintTime;
+    private String status;
+    private int handlerId;
+    private String handleInfo;
+    private Date handleTime;
+
+    public Complaint() {
+        // 默认构造函数
+    }
+
+    public Complaint(int id, int complainerId, int canteenId, String complaintInfo,
+                     Date complaintTime, String status, int handlerId, String handleInfo, Date handleTime) {
+        this.id = id;
+        this.complainerId = complainerId;
+        this.canteenId = canteenId;
+        this.complaintInfo = complaintInfo;
+        this.complaintTime = complaintTime;
+        this.status = status;
+        this.handlerId = handlerId;
+        this.handleInfo = handleInfo;
+        this.handleTime = handleTime;
+    }
+
+    // getter和setter方法
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getComplainerId() {
+        return complainerId;
+    }
+
+    public void setComplainerId(int complainerId) {
+        this.complainerId = complainerId;
+    }
+
+    public int getCanteenId() {
+        return canteenId;
+    }
+
+    public void setCanteenId(int canteenId) {
+        this.canteenId = canteenId;
+    }
+
+    public String getComplaintInfo() {
+        return complaintInfo;
+    }
+
+    public void setComplaintInfo(String complaintInfo) {
+        this.complaintInfo = complaintInfo;
+    }
+
+    public Date getComplaintTime() {
+        return complaintTime;
+    }
+
+    public void setComplaintTime(Date complaintTime) {
+        this.complaintTime = complaintTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getHandlerId() {
+        return handlerId;
+    }
+
+    public void setHandlerId(int handlerId) {
+        this.handlerId = handlerId;
+    }
+
+    public String getHandleInfo() {
+        return handleInfo;
+    }
+
+    public void setHandleInfo(String handleInfo) {
+        this.handleInfo = handleInfo;
+    }
+
+    public Date getHandleTime() {
+        return handleTime;
+    }
+
+    public void setHandleTime(Date handleTime) {
+        this.handleTime = handleTime;
+    }
+}
+
